825d00f7a6664b651ed7ddb60d266911af4ea64b
Wellington Correia <wellington.dossantos@montreal.com.br>
2021-03-15 12:14:17 -0300

    Configured Avro.

---
+++ checkout-api/src/test/java/com/wellington/ecommerce/checkoutapi/CheckoutApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wellington.ecommerce.checkoutapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckoutApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
